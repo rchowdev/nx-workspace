efeb68223f37a5a94bf07d6adde6defdecf84d5f
Raymond Chow <rchow2@buffalo.edu>
2020-02-29 03:11:17 -0500

    initial commit

---
+++ apps/tv-shows/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: 'tv-shows',
+  preset: '../../jest.config.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
+  coverageDirectory: '../../coverage/apps/tv-shows'
+};
